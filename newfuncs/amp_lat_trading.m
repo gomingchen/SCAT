1ea1ae849b3bcc20a2967c665eec2c222770fd02
gomingchen <55762370+gomingchen@users.noreply.github.com>
2020-02-28 12:49:49 -0500

    Update amp_lat_trading.m
    Optimized the amplitude-finding process that are used to determine the amplitude difference between pulse and echo

--- newfuncs/amp_lat_trading.m
+++ newfuncs/amp_lat_trading.m
@@ -14,6 +14,10 @@ thd = max(yu(kk(1):end))*ra;
 [amps,~] = findpeaks(yu(kk(1):end), 'NPeaks', 2, 'MinPeakHeight',thd);
 
 if length(amps) == 2
+    % make sure the peaks found are true peak values of pulse and echo
+    amps(1) = max(amps(1), max(yu(kk(1):kk(1)+locs(1)+500)));
+    amps(2) = max(amps(2), max(yu(kk(1)+locs(2)-500: kk(1)+locs(2)+500)));
+    %
     delta_db = 20*log10(amps(1)/amps(2)); % (amp_of_pulse)/(amp_of_echo)
     time_in_us = delta_db*ALT_coef; % latency increase due to the amplitude decrease
 else
